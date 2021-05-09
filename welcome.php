71dae6688c825fcf462250f433ceb6304ee2177e
armanhn <arman.hn007@gmail.com>
2021-05-10 02:42:32 +0600

    new file

---
+++ welcome.php
@@ -0,0 +1,15 @@
+<?php
+   include('connection.php');
+?>
+<html">
+   
+   <head>
+      <title>Welcome faculty</title>
+   </head>
+   
+   <body>
+      <h1>Welcome Sir/Ma'am </h1> 
+      <h2><a href = "logout.php">Sign Out</a></h2>
+   </body>
+   
+</html>
---
+++ welcome_student.php
@@ -0,0 +1,15 @@
+<?php
+   include('connection.php');
+?>
+<html">
+   
+   <head>
+      <title>Welcome student</title>
+   </head>
+   
+   <body>
+      <h1>Welcome student </h1> 
+      <h2><a href = "logout.php">Sign Out</a></h2>
+   </body>
+   
+</html>
