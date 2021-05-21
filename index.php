0ce719562e8e6a1ad56af9b37e4e2fae2aa1d50e
mahdiafaiza <mideverything@outlook.com>
2021-05-21 14:23:57 +0600

    updated file

--- index.php
+++ index.php
@@ -19,8 +19,8 @@
 			$num_row2 = mysqli_num_rows($result2);
 
 			$sql3 = "SELECT  * FROM dept WHERE dept_head_id = '$id' and password = '$password'";
-			$result3 = mysqli_query($conn,$sql3);
-			$num_row3 = mysqli_num_rows($result3);
+            $result3 = mysqli_query($conn,$sql3);
+            $num_row3 = mysqli_num_rows($result3);
 			
 			if($num_row1 > 0 ) {
 					$data = mysqli_fetch_array($result1);
@@ -51,26 +51,33 @@
 <!DOCTYPE html>
 <html>
 <head>
+<!-- CSS only -->
+
+
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+
 	<title>Login page</title>
 	<style>
   		#main {
+			box-shadow: 0 0 10px 5px grey;
+			background-color: transparent;
   			width:500px;
-  			margin: 300px auto;
+  			margin: 110px auto;
+			line-height: 40px;
 			text-align: center;
   			padding: 5px;
   			border-radius: 5px;
-  			border:2px solid #2c3e50;
-			
-
+  			border:2px;
   		}
 		.a{
-			background: url("pic5.jpeg");
+			background: url("lightpattern.png");
 			background-size: cover;
 			background-position: center;
 			font-family: sans-serif;
 		}
   		.iublogo {
-  			width: 100px;
+  			width: 200px;
   			text-align: center;
   			border-radius: 50%;
   		}
@@ -86,9 +93,13 @@
   			padding: 5px;
 
   		}
+		.login_btn:hover {
+			background-color: black;
+			color: white;
+}
   		.login_btn{
   			margin-top: 10px;
-  			background: #E77471;
+  			background: grey;
 			color: #fff;
 			padding: 5px;	
   			width: 100px;
@@ -96,6 +107,7 @@
   			font-size: 18px;
   			font-weight: bold;
   			margin-bottom: 20px;
+			border-radius: 20px;
   		}
   		.register_btn{
   			margin-top: 20px;
@@ -115,36 +127,56 @@
   			margin-top: 20px;
   			background-color: white;
   		}
+		.lol{
+			margin-left: -373px;
+		}
+		.lmao{
+			margin-left: -390px;
+		}
+		.header {
+  			padding: 20px;
+			text-align: center;
+			color: black;
+			font-size: 25px;
+		}
+	
 
 	</style>
 
 </head>
+		<div class="header">
+  		<h1>VYKON</h1>
+		</div>
 <body class = "a" >
-		
+    
+	
+
 	<div id="main">
 	 <center>
-       <h2>Sign in</h2>
-       <img src="iublogo.png" class="iublogo">
+       <h2>Sign In</h2>
+       <img src="iub_logo.png" class="iublogo">
      </enter>
 
       <form class="my_form" action="index.php"  method="post">
-       	<label>User ID:</label><br>
+       	<div class = "lmao">
+		   <label>User ID</label><br>
+		   </div>
          <input type="text" class="inputevalues" name="id" id="id" placeholder="Enter Username"><br>
+		 <div class = "lol">
          <label>Password</label><br>
+		 </div>
          <input type="password" class="inputevalues" name="password" id="password" placeholder="Type Your Password"><br>
 	
 
           <input type="submit" class="login_btn" name="login" id="login" value="Login"><br>
 
        </form>
+	 </center>
 
-
-
-	   
-	   
 	 
 	</div>
 	
    
 	</body>
 </html>
+
