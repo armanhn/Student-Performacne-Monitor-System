7115d1845a32caa194639f6e9b198ca006002757
armanhn <arman.hn007@gmail.com>
2021-05-21 19:02:50 +0600

    added new charts

---
+++ student_wise_radar.php
@@ -0,0 +1,20 @@
+<?php
+	include "connection.php"
+	session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Dropdown list</title>
+</head>
+<body>
+		<form action = "student_wise_radar_chart.php" method = "post">
+		<th>Enter Stundent ID</th>
+		<td><input type = "text" name = "id" ></td>
+		<br>
+		<td><input type = "submit" name = "submit" ></td>
+		<br>
+
+</body>
+</html>
--- student_wise_radar_chart.php
+++ student_wise_radar_chart.php
@@ -1,15 +1,14 @@
 <?php
-
-//session_start();
 include "connection.php";
+session_start();
 require_once ('D:/XAMPP/htdocs/jpgraph-4.3.4/jpgraph-4.3.4/src/jpgraph.php');
-require_once ('D:/XAMPP/htdocs/jpgraph-4.3.4/jpgraph-4.3.4/src/jpgraph_bar.php');
+require_once ('D:/XAMPP/htdocs/jpgraph-4.3.4/jpgraph-4.3.4/src/jpgraph_log.php');
+require_once ('D:/XAMPP/htdocs/jpgraph-4.3.4/jpgraph-4.3.4/src/jpgraph_radar.php');
 
-
-
-   
-
-    $grand_total=0; // Total count of plo achieved
+if(isset($_POST['submit']))
+{	
+	$id = $_POST['id'];
+	$grand_total=0; // Total count of plo achieved
     for($k=1;$k<=13;$k++) /// loop will run all the 13 plo
     {
         $plo = "plo".$k;  // converting plo into plo1-plo13.
@@ -58,15 +57,14 @@ require_once ('D:/XAMPP/htdocs/jpgraph-4.3.4/jpgraph-4.3.4/src/jpgraph_bar.php')
                             {
                         $sql2 = "SELECT SUM(CASE WHEN $CLO IS NOT NULL THEN 1 ELSE 0 END) AS ccount
                         FROM grade_sheet AS g, plo_table AS p
-                        WHERE g.serial_id = p.serial_id AND g.id = '1711118' AND g.$CLO = 'Y' AND p.$plo = '$ploValue'";
+                        WHERE g.serial_id = p.serial_id AND g.id = '$id' AND g.$CLO = 'Y' AND p.$plo = '$ploValue'";
 
                         $result2 = mysqli_query($conn,$sql2);
 
                         $row2 = mysqli_fetch_assoc($result2);
 
                         $value = $row2['ccount'];
-                        //echo $value;
-                       //echo "<br>";
+
                         $add += $value; // storing the data in a variable
                             }   
 
@@ -76,24 +74,14 @@ require_once ('D:/XAMPP/htdocs/jpgraph-4.3.4/jpgraph-4.3.4/src/jpgraph_bar.php')
                 }
                 
 
-            //echo $plo." TOTAL = ";
-            //echo $add."<br>";/// prinitng the summation of total achieved plo
+
             $grand_total += $add; 
             $plowise[$k] =$add; /// storing the data in array for future use
     }
-            //echo "<br>";
-            //echo"<hr>";
-            //echo"TOTAL = ";
-            //echo $grand_total;
-            //echo"<br>";
 
 
-    for($i=1;$i<=13;$i++)
-    {
-        echo $plowise[$i];
-        echo "<br>";
-    }
-echo "<hr>";
+
+
     $grand_total=0; // Total count of plo achieved
     for($k=1;$k<=13;$k++) /// loop will run all the 13 plo
     {
@@ -150,8 +138,7 @@ echo "<hr>";
                         $row2 = mysqli_fetch_assoc($result2);
 
                         $value = $row2['ccount'];
-                        //echo $value;
-                       //echo "<br>";
+;
                         $add += $value; // storing the data in a variable
                             }   
 
@@ -161,30 +148,64 @@ echo "<hr>";
                 }
                 
 
-            //echo $plo." TOTAL = ";
-            //echo $add."<br>";/// prinitng the summation of total achieved plo
+
             $grand_total += $add; 
             $plowise2[$k] =$add; /// storing the data in array for future use
     }
+    
+    
 
 
-    for($i=1;$i<=13;$i++)
-    {
-        echo $plowise2[$i];
-        echo "<br>";
-    }
+
+
  
 
+ 
+// Create the basic rtadar graph
+$graph = new RadarGraph(800,600);
+ 
+// Set background color and shadow
+$graph->SetColor("white");
+$graph->SetShadow();
+ 
+// Position the graph
+$graph->SetCenter(0.4,0.55);
+ 
+// Setup the axis formatting     
+$graph->axis->SetFont(FF_FONT1,FS_BOLD);
+$graph->axis->SetWeight(2);
+ 
+// Setup the grid lines
+$graph->grid->SetLineStyle("longdashed");
+$graph->grid->SetColor("navy");
+$graph->grid->Show();
+$graph->HideTickMarks();
+        
+// Setup graph titles
+$graph->title->Set("Quality result");
+$graph->title->SetFont(FF_FONT1,FS_BOLD);
+$graph->SetTitles(array("One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Eleven","Twelve","Thirteen"));
+// Create the first radar plot        
+$plot = new RadarPlot(array($plowise2[1],$plowise2[2],$plowise2[3],$plowise2[4],$plowise2[5],$plowise2[6],$plowise2[7],$plowise2[8],$plowise2[9],$plowise2[10],$plowise2[11],$plowise2[12],$plowise2[13]));
+$plot->SetLegend("Goal");
+$plot->SetColor("red","lightred");
+$plot->SetFill(false);
+$plot->SetLineWeight(2);
+ 
+// Create the second radar plot
+$plot2 = new RadarPlot(array($plowise[1],$plowise[2],$plowise[3],$plowise[4],$plowise[5],$plowise[6],$plowise[7],$plowise[8],$plowise[9],$plowise[10],$plowise[11],$plowise[12],$plowise[13]));
+$plot2->SetLegend("Actual");
+$plot2->SetColor("blue","lightred");
+ 
+// Add the plots to the graph
+$graph->Add($plot2);
+$graph->Add($plot);
+ 
+// And output the graph
+$graph->Stroke();
 
 
-
-
-
-
-
-
-
-
+}
 
 
 ?>
