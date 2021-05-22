b510bcd500d154377f8f69d7ca313b8e240b077a
sakibraihan47 <sakibraihan47@gmail.com>
2021-05-22 23:33:28 +0600

    added dept head assessment page and edited the quick links

--- dept_head_dash.php
+++ dept_head_dash.php
@@ -155,7 +155,7 @@
                                 <div class="card bg-dark text-white mb-4">
                                     <div class="card-body">ASSESSMENT</div>
                                     <div class="card-footer d-flex align-items-center justify-content-between">
-                                        <a class="small text-white stretched-link" href="">View Details</a>
+                                        <a class="small text-white stretched-link" href="head_assessment.php">View Details</a>
                                         <div class="small text-white"><i class="fas fa-angle-right"></i></div>
                                     </div>
                                 </div>
