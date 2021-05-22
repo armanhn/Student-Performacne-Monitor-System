dab173be1638faf60392280bede774c7da9426b6
mahdiafaiza <mideverything@outlook.com>
2021-05-23 05:08:07 +0600

    updated final plo

--- plo_mapping.php
+++ plo_mapping.php
@@ -6,22 +6,170 @@
          $id = $_SESSION['id'];
 ?>
 <!DOCTYPE html>
-<html>
-<head>
-	<title>plo mapping</title>
-</head>
-<body>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>PLO MAPPING</title>
+        <link href="css/styles.css" rel="stylesheet" />
+        <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
 
-	
-	<form method = "post">
+        <style>
+            .death{
+               
+                background:#f4eee8;
+                border-radius: 10px;
+                padding: 20px;
+				margin:auto;
+             }
+             .liv1{
+            
+               background: ;
+            }
+            .liv2{
+               margin-left: 500px;
+               margin-top: -278px;
+            
+            }
+            .liv3{
+               margin-left: 500px;
+               margin-top: -278px;
+               background: ;
+            }
+            .liv4{
+               margin-left: 500px;
+               margin-top: -278px;
+               background: ;
+            }
+            .liv5{
+               margin-left: -1500px;
+               margin-top: 40px;
+               background: ;
+            }
+            .liv6{
+               margin-left: 500px;
+               margin-top: -278px;
+               background: ;
+            }
+            .liv7{
+               margin-left: 500px;
+               margin-top: -278px;
+               background: ;
+            }
+            .liv8{
+               margin-left: 500px;
+               margin-top: -278px;
+               background: ;
+            }
+            .liv9{
+               margin-left: -1500px;
+               margin-top: 40px;
+            }
+            .liv10{
+               margin-left: 500px;
+               margin-top: -278px;
+               background: ;
+            }
+            .liv11{
+               margin-left: 500px;
+               margin-top: -278px;
+               background: ;
+            }
+            .liv12{
+               margin-left: 500px;
+               margin-top: -278px;
+               background: ;
+            }
+            .liv13{
+               margin-left:-1500px;
+               margin-top: 40px;
+               background: ;
+            }
+            input[type=submit] {
+			margin-left: 770px;
+   			 width: 20em;  height: 2em;
+			
+				}
+            
+			input[type=submit]:hover {
+    		background-color:#45526c;
+    		color: white;
+  }
+        </style>
 
-		<h3>Select Course ID</h3>
-		<select name = "section_name" >
-			<?php
+    </head>
+    <body class="sb-nav-fixed">
+        <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+            <a class="navbar-brand" href="facultydash.php">VYKON DASHVIEW</a>
+
+            <form>
+                <button class="btn" style="border: none;
+                background: peachpuff;
+                margin: 10px;
+                outline: none;
+                color:black;
+                font-size: 16px;" 
+                
+                formaction="facultydash.php" >HOME</button>
+              </form>
+            <!-- LOGOUT BUTTN-->
+            <form>
+                <button class="btn" style="border: none;
+                background: rgb(167, 10, 10);
+                background-position: right;
+                margin: 5px;; 
+                outline: none;
+                color:white;
+                font-size: 16px;" 
+                formaction="logout.php" >LOGOUT</button>
+              </form>
+
+
+
+            
+            <!-- Navbar-->
+            <ul class="navbar-nav ml-auto ml-md-0"></ul>
+        </nav>
+        
+                            <a class="nav-link" href="tables.html">
+                                <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                                Tables
+                            </a>
+                        </div>
+                    </div>
+                    
+                </nav>
+            </div>
+            <div id="layoutSidenav_content">
+                <main>
+                    <div class="container-fluid">
+                        <h1 class="mt-4">PLO MAPPING</h1>
+                        <ol class="breadcrumb mb-4">
+                            <li class="breadcrumb-item"><a href="facultydash.php">Dashboard</a></li>
+                            <li class="breadcrumb-item active">PLO MAPPING</li>
+                        </ol>
+                        <div class="card mb-4">
+                            <div class="card-body">
+                                SELECT CLO's for Respective PLO's
+                            
+                            </div>
+                        </div>
+                        
+
+        <div class="death">
+
+                         <form method = "post">
+	    <h3>Select Course ID</h3>
+	    <select name = "section_name" >
+	    <?php
 
 				$sql= "SELECT section_name FROM section WHERE faculty_id = '$id'";
 				$result = mysqli_query($conn,$sql);
-
+				echo "<option value= '--select--'>--select--</option>";
 				while($rows =  mysqli_fetch_assoc($result))
 				{
 					$section_name = $rows['section_name'];
@@ -30,7 +178,11 @@
 
 			?> 
 		</select>
+        <div>
+        </div>
 		<br>
+        
+        <div class="liv1">
 		<h3>FOR PLO 1 </h3>
 		<p><input type = "checkbox" name= "plo1[]" value= "CLO1"/> CLO1</p>
 		<p><input type = "checkbox" name= "plo1[]" value= "CLO2"/> CLO2</p>
@@ -39,6 +191,9 @@
 		<p><input type = "checkbox" name= "plo1[]" value= "CLO5"/> CLO5</p>
 		<p><input type = "checkbox" name= "plo1[]" value= "CLO6"/> CLO6</p>
 		<p></p>
+		
+
+        <div class="liv2">
 		<h3>FOR PLO 2 </h3>
 		<p><input type = "checkbox" name= "plo2[]" value= "CLO1"/> CLO1</p>
 		<p><input type = "checkbox" name= "plo2[]" value= "CLO2"/> CLO2</p>
@@ -47,6 +202,8 @@
 		<p><input type = "checkbox" name= "plo2[]" value= "CLO5"/> CLO5</p>
 		<p><input type = "checkbox" name= "plo2[]" value= "CLO6"/> CLO6</p>
 		<p></p>
+
+        <div class="liv3">
 		<h3>FOR PLO 3 </h3>
 		<p><input type = "checkbox" name= "plo3[]" value= "CLO1"/> CLO1</p>
 		<p><input type = "checkbox" name= "plo3[]" value= "CLO2"/> CLO2</p>
@@ -55,6 +212,8 @@
 		<p><input type = "checkbox" name= "plo3[]" value= "CLO5"/> CLO5</p>
 		<p><input type = "checkbox" name= "plo3[]" value= "CLO6"/> CLO6</p>
 		<p></p>
+
+        <div class="liv4">
 		<h3>FOR PLO 4 </h3>
 		<p><input type = "checkbox" name= "plo4[]" value= "CLO1"/> CLO1</p>
 		<p><input type = "checkbox" name= "plo4[]" value= "CLO2"/> CLO2</p>
@@ -63,6 +222,8 @@
 		<p><input type = "checkbox" name= "plo4[]" value= "CLO5"/> CLO5</p>
 		<p><input type = "checkbox" name= "plo4[]" value= "CLO6"/> CLO6</p>
 				<p></p>
+
+        <div class="liv5">
 		<h3>FOR PLO 5 </h3>
 		<p><input type = "checkbox" name= "plo5[]" value= "CLO1"/> CLO1</p>
 		<p><input type = "checkbox" name= "plo5[]" value= "CLO2"/> CLO2</p>
@@ -71,6 +232,8 @@
 		<p><input type = "checkbox" name= "plo5[]" value= "CLO5"/> CLO5</p>
 		<p><input type = "checkbox" name= "plo5[]" value= "CLO6"/> CLO6</p>
 				<p></p>
+
+        <div class="liv6">       
 		<h3>FOR PLO 6 </h3>
 		<p><input type = "checkbox" name= "plo6[]" value= "CLO1"/> CLO1</p>
 		<p><input type = "checkbox" name= "plo6[]" value= "CLO2"/> CLO2</p>
@@ -79,6 +242,8 @@
 		<p><input type = "checkbox" name= "plo6[]" value= "CLO5"/> CLO5</p>
 		<p><input type = "checkbox" name= "plo6[]" value= "CLO6"/> CLO6</p>
 				<p></p>
+
+        <div class="liv7">       
 		<h3>FOR PLO 7 </h3>
 		<p><input type = "checkbox" name= "plo7[]" value= "CLO1"/> CLO1</p>
 		<p><input type = "checkbox" name= "plo7[]" value= "CLO2"/> CLO2</p>
@@ -87,6 +252,8 @@
 		<p><input type = "checkbox" name= "plo7[]" value= "CLO5"/> CLO5</p>
 		<p><input type = "checkbox" name= "plo7[]" value= "CLO6"/> CLO6</p>
 				<p></p>
+
+        <div class="liv8"> 
 		<h3>FOR PLO 8 </h3>
 		<p><input type = "checkbox" name= "plo8[]" value= "CLO1"/> CLO1</p>
 		<p><input type = "checkbox" name= "plo8[]" value= "CLO2"/> CLO2</p>
@@ -95,6 +262,8 @@
 		<p><input type = "checkbox" name= "plo8[]" value= "CLO5"/> CLO5</p>
 		<p><input type = "checkbox" name= "plo8[]" value= "CLO6"/> CLO6</p>
 				<p></p>
+
+         <div class="liv9">       
 		<h3>FOR PLO 9 </h3>
 		<p><input type = "checkbox" name= "plo9[]" value= "CLO1"/> CLO1</p>
 		<p><input type = "checkbox" name= "plo9[]" value= "CLO2"/> CLO2</p>
@@ -103,6 +272,8 @@
 		<p><input type = "checkbox" name= "plo9[]" value= "CLO5"/> CLO5</p>
 		<p><input type = "checkbox" name= "plo9[]" value= "CLO6"/> CLO6</p>
 				<p></p>
+
+         <div class="liv10">
 		<h3>FOR PLO 10 </h3>
 		<p><input type = "checkbox" name= "plo10[]" value= "CLO1"/> CLO1</p>
 		<p><input type = "checkbox" name= "plo10[]" value= "CLO2"/> CLO2</p>
@@ -111,6 +282,8 @@
 		<p><input type = "checkbox" name= "plo10[]" value= "CLO5"/> CLO5</p>
 		<p><input type = "checkbox" name= "plo10[]" value= "CLO6"/> CLO6</p>
 				<p></p>
+
+         <div class="liv11">
 		<h3>FOR PLO 11 </h3>
 		<p><input type = "checkbox" name= "plo11[]" value= "CLO1"/> CLO1</p>
 		<p><input type = "checkbox" name= "plo11[]" value= "CLO2"/> CLO2</p>
@@ -119,6 +292,8 @@
 		<p><input type = "checkbox" name= "plo11[]" value= "CLO5"/> CLO5</p>
 		<p><input type = "checkbox" name= "plo11[]" value= "CLO6"/> CLO6</p>
 				<p></p>
+
+         <div class="liv12">
 		<h3>FOR PLO 12 </h3>
 		<p><input type = "checkbox" name= "plo12[]" value= "CLO1"/> CLO1</p>
 		<p><input type = "checkbox" name= "plo12[]" value= "CLO2"/> CLO2</p>
@@ -127,6 +302,8 @@
 		<p><input type = "checkbox" name= "plo12[]" value= "CLO5"/> CLO5</p>
 		<p><input type = "checkbox" name= "plo12[]" value= "CLO6"/> CLO6</p>
 				<p></p>
+          
+        <div class="liv13">
 		<h3>FOR PLO 13 </h3>
 		<p><input type = "checkbox" name= "plo13[]" value= "CLO1"/> CLO1</p>
 		<p><input type = "checkbox" name= "plo13[]" value= "CLO2"/> CLO2</p>
@@ -134,8 +311,8 @@
 		<p><input type = "checkbox" name= "plo13[]" value= "CLO4"/> CLO4</p>
 		<p><input type = "checkbox" name= "plo13[]" value= "CLO5"/> CLO5</p>
 		<p><input type = "checkbox" name= "plo13[]" value= "CLO6"/> CLO6</p>
-
-		<input type = "submit" name= "submit" value= "submit"/>
+				
+		<input type = "submit" name= "submit" value= "SUBMIT"/>
 	
 	<?php
 
@@ -284,7 +461,8 @@
 
 			if(mysqli_query($conn,$sql))
 				{
-					echo"done";
+					echo"DONE";
+                    //done under new class should work with submit tag
 				}
 				else
 				{
@@ -294,11 +472,30 @@
 		}
 
 	?>
+   
 
-
-
-
-
-
-</body>
+                    </div>
+                    
+                </main>
+                <footer class="py-4 bg-light mt-auto">
+                    <div class="container-fluid">
+                        <div class="d-flex align-items-center justify-content-between small">
+                            <div class="text-muted">GROUP6 &copy; vykon-cse303-GR6</div>
+                            <div>
+                                <a href="#">Privacy Policy</a>
+                                &middot;
+                                <a href="#">Terms &amp; Conditions</a>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+            </div>
+        </div>
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="js/scripts.js"></script>
+        <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
+        <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
+        <script src="assets/demo/datatables-demo.js"></script>
+    </body>
 </html>
